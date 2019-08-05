b048cd3895cde6c2f9bab7c19a6ec346732cd4e9
Yifan1989 <j.codes1989@gmail.com>
2019-08-05 15:47:13 +1200

    multiple migrations for many tables

--- Uni_Dash/Uni_Dash/Models/Login.cs
+++ Uni_Dash/Uni_Dash/Models/Login.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace Uni_Dash.Models
 {
     public class Login
     {
+        [Key]
         public string user { get; set; }
         public string passwd { get; set; }
         public Login()

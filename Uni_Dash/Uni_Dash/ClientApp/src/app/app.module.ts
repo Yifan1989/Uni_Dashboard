3fd81bc75ef48ba378c25195156e368e041ae1f9
Yifan1989 <j.codes1989@gmail.com>
2019-08-05 23:53:37 +1200

    front end done

--- Uni_Dash/Uni_Dash/ClientApp/src/app/app.module.ts
+++ Uni_Dash/Uni_Dash/ClientApp/src/app/app.module.ts
@@ -1,8 +1,9 @@
-import { BrowserModule } from '@angular/platform-browser';
+﻿import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
+
 
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
@@ -22,6 +23,7 @@ import { FetchDataComponent } from './fetch-data/fetch-data.component';
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
     FormsModule,
+    ReactiveFormsModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
       { path: 'counter', component: CounterComponent },
--- Uni_Dash/Uni_Dash/ClientApp/src/app/home/home.component.ts
+++ Uni_Dash/Uni_Dash/ClientApp/src/app/home/home.component.ts
@@ -1,8 +1,24 @@
-import { Component } from '@angular/core';
+﻿import { Component, OnInit } from '@angular/core';
+import { Validators, FormGroup, FormControl, FormBuilder } from '@angular/forms';
+
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
+
+  userForm : FormGroup;
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit(): void{
+    this.userForm = this.fb.group({
+      user: [""], //initilize ob not using =
+      passwd: [""],
+    });
+  }
+
+    test() {
+        console.log(this.userForm.value);
+    }
 }

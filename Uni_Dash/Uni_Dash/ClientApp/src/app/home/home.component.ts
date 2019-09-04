b2899a92182d34c9f2c0157314d7023a1506bde3
Yifan1989 <j.codes1989@gmail.com>
2019-09-04 18:09:40 +1200

    post good

--- Uni_Dash/Uni_Dash/ClientApp/src/app/home/home.component.ts
+++ Uni_Dash/Uni_Dash/ClientApp/src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 ﻿import { Component, OnInit } from '@angular/core';
 import { Validators, FormGroup, FormControl, FormBuilder } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 
 @Component({
@@ -7,10 +8,22 @@ import { Validators, FormGroup, FormControl, FormBuilder } from '@angular/forms'
   templateUrl: './home.component.html',
 })
 export class HomeComponent implements OnInit{
+  postData = {
+      user: "new user",
+      passwd: "111111"
+  }
+
+  url = 'https://localhost:5001/api/login';
 
   userForm : FormGroup;
-  constructor(private fb: FormBuilder) { }
+  constructor(private fb: FormBuilder, private http: HttpClient) {
+      this.http.post(this.url, this.postData).toPromise().then(data => {
+          console.log(data);
+      });
+  }
 
+ 
+  
   ngOnInit(): void{
     this.userForm = this.fb.group({
       user: [""], //initilize ob not using =
@@ -20,5 +33,7 @@ export class HomeComponent implements OnInit{
 
     test() {
         console.log(this.userForm.value);
+
+
     }
 }

974c9f9bf5653f6d83f1be790b1defc0a672ef55
Yifan1989 <j.codes1989@gmail.com>
2019-09-09 19:33:00 +1200

    ef done

--- Uni_Dash/Uni_Dash/Migrations/20190909072907_HashLogin.Designer.cs
+++ Uni_Dash/Uni_Dash/Migrations/20190909072907_HashLogin.Designer.cs
@@ -2,15 +2,17 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Uni_Dash.Data;
 
 namespace Uni_Dash.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20190909072907_HashLogin")]
+    partial class HashLogin
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Uni_Dash/Uni_Dash/Migrations/20190909072907_HashLogin.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Uni_Dash.Migrations
+{
+    public partial class HashLogin : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "LoginHashs",
+                columns: table => new
+                {
+                    user = table.Column<string>(nullable: false),
+                    pwd = table.Column<string>(nullable: true),
+                    hashPwd = table.Column<string>(nullable: true),
+                    role = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LoginHashs", x => x.user);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "LoginHashs");
+        }
+    }
+}
